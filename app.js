9a4c628e0764ec599417ff76daa7f0bd1c69ea30
wadi2a <ben.aissa.ouadie@gmail.com>
2019-10-11 11:16:17 +0200

    organisation des dossiers

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use((req,res,next) => {
   next();
 })
 app.use('/', require('./routes/index'));
-app.use('/users', require('./routes/users'));
+app.use('/users', require('./Ressources/User/routeUser'));
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
